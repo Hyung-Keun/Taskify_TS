8bff10d71bbbc4c7ceca2b9d5f50d03afe316abc
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-29 01:02:09 +0900

    Feature: inputfield에 todolist 항목 추가 및 수정.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  console.log(inputRef);
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
